8a815e5c0e8320fbc95f9ea648d918fa33b5d381
JoseRodriguezJ <jl.rodriguez@iesdoctorbalmis.com>
2021-10-18 23:01:25 +0200

    Agregar archivos de proyecto.

---
+++ Calculadora/MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Calculadora
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            resultadoTextBlock.Text += ((Button)sender).Tag.ToString();
+        }
+    }
+}
